8c69ab376d445839a77d096bb608997ed2553e5b
MrAnyx <1ptitigre@gmail.com>
2020-09-28 21:16:25 +0200

    [fix] Fixed component path

--- src/Script.php
+++ src/Script.php
@@ -9,20 +9,20 @@ class Script {
    public static function postPackageInstall(PackageEvent $event) {
       $installedPackage = str_replace("/", ":", $event->getOperation()->getPackage()->getName());
 		if(explode(":", $installedPackage)[0] === "timephp"){
-      	$content = json_decode(file_get_contents(__DIR__ . "/../bin/components.json"), true);
+      	$content = json_decode(file_get_contents(__DIR__ . "/../../../../bin/components.json"), true);
 			$content[] = $installedPackage;	
-			file_put_contents(__DIR__ . "/../bin/components.json", json_encode($content, JSON_PRETTY_PRINT));
+			file_put_contents(__DIR__ . "/../../../../bin/components.json", json_encode($content, JSON_PRETTY_PRINT));
 		}
    }
 
    public static function prePackageUninstall(PackageEvent $event) {
       $uninstalledPackage = str_replace("/", ":", $event->getOperation()->getPackage()->getName());
 		if(explode(":", $uninstalledPackage)[0] === "timephp"){
-      	$content = json_decode(file_get_contents(__DIR__ . "/../bin/components.json"), true);
+      	$content = json_decode(file_get_contents(__DIR__ . "/../../../../bin/components.json"), true);
       	if (($key = array_search($uninstalledPackage, $content)) !== false) {
 				unset($content[$key]);
 		  	}
-			file_put_contents(__DIR__ . "/../bin/components.json", json_encode($content, JSON_PRETTY_PRINT));
+			file_put_contents(__DIR__ . "/../../../../bin/components.json", json_encode($content, JSON_PRETTY_PRINT));
 		}
    }
 }
